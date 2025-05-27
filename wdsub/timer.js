0595ce32e750e3c61852a9826f0ba74d41513a50
yacs-1 <chromebook1@youngsvilleacademy.org>
2025-05-27 15:16:56 -0400

    Update timer.js

--- wdsub/timer.js
+++ wdsub/timer.js
@@ -7,7 +7,7 @@ cookieArr = document.cookie.split(";");
 for (const element of cookieArr) {
   if (element.split("=")[0] == "init-date") {
     hasInitDate = true;
-    initDate = element.split("=")[1];
+    initDate = new Date(element.split("=")[1]);
   }
 }
 
@@ -15,7 +15,7 @@ if (!hasInitDate) {
   document.cookie = "init-date=" + new Date() + "; max-age=31536000000";
 } else {
   let now = new Date();
-  document.cookie = "new-date" + now + "; max-age=31536000000";
+  document.cookie = "new-date=" + now + "; max-age=31536000000";
   timeDifference = now.getTime() - initDate.getTime(); //gets the difference in milliseconds from first access until now
 }
 
